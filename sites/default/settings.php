955d9b44ee7a299f844a11d0f44945f4b43471e3
Mohamed Abdoul Karim DIOP [IPD DDSI/DIF] <mohamedabdoulkarim.diop@pasteur.sn>
2025-02-22 18:28:08 +0000

    dump db

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -706,7 +706,8 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  */
 $settings['trusted_host_patterns'] = [
   '^intek\.sn$',
-  '^.+\.intek\.sn$'
+  '^.+\.intek\.sn$',
+  '^localhost$',
  ];
 
 
@@ -774,14 +775,14 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-# $databases['default']['default'] = array (
-#  'database' => '',
-#  'username' => '',
-#  'password' => '',
-#  'prefix' => '',
-#  'host' => '',
-#  'port' => '3306',
-#  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-#  'driver' => 'mysql',
+ $databases['default']['default'] = array (
+  'database' => 'inteksi324',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
 );
 $settings['config_sync_directory'] = 'sites/default/files/config_TlledbIS4xuejMZ5enNSVV175Uv99vLH1VydkXpQTXSx4_Ft_iW0KNUSLjD96-hp4jsnlhDLzg/sync';
